bcf514d2755cff5d2ce565fcfcc5515df56196da
wigothehacker <wigothehacker@gmail.com>
2025-11-12 22:00:45 +0200

    Did small excercise

--- tut.playground/Pages/swiftBasics.xcplaygroundpage/Contents.swift
+++ tut.playground/Pages/swiftBasics.xcplaygroundpage/Contents.swift
@@ -1,4 +1,39 @@
+let name = "Goal"
+print("Hello, \(name)!")
 
-import Foundation
+let age = 17
+print("You are \(age) years old.")
 
-var greeting = "Hello, playground"
+let num1 = 10
+let num2 = 20
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+var isStudent = false
+
+print(isStudent ? "You are a Student" : "You are not a Student")
+
+//Level 2
+
+let country = "Rwanda"
+
+if(country == "Rwanda"){
+    print( "You live in Rwanda" )
+}else{
+    print("You live somewhere else")
+}
+
+let temperature = 50
+
+if temperature > 30 {
+    print("Hot")
+}else if( temperature > 20) {
+    print("Warm")
+}else {
+    print("Cold")
+}
+
+
+ 
